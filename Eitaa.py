2f496c39af2841ec2d2cfbe2b777267e221132f1
TrustInPy <sinasolimany@gmail.com>
2023-10-24 14:49:39 +0330

    commit

--- Eitaa.py
+++ Eitaa.py
@@ -8,14 +8,14 @@ telegram_client = TelegramClient('aaa2', '25807486', 'ba12a961667ee37788fc5521be
 
 token = 'bot83779:55b9fbae-1a76-4731-9add-173dbc02b839'
 chat_id = 'twiper'
-replacement_text = 'your_replacement_text'
+replacement_text = '@twiper'
 
 @telegram_client.on(events.NewMessage(chats=('OfficialPersianTwitter')))
 async def telegram_event_handler(event):
     message = event.message.text
 
     if '@' in message:
-        message = re.sub(r'@\w+', '@twiper', message)
+        message = re.sub(r'@\w+', replacement_text, message)
 
     url = f'https://eitaayar.ir/api/{token}/sendMessage'
 
@@ -25,7 +25,6 @@ async def telegram_event_handler(event):
 
     data = {
         'chat_id': chat_id,
-        # 'title': title,
         'text': message,
         'date': time.time() + 2 
     }
