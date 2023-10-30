57932c63bd2f4e812c902b6e0e4194035814aa23
TrustInPy <sinasolimany@gmail.com>
2023-10-30 16:03:02 +0330

    semi_final

--- Eitaa copy.py
+++ Eitaa copy.py
@@ -8,8 +8,9 @@ from dotenv import load_dotenv
 from telethon import TelegramClient, events
 
 load_dotenv()
-CHAT_ID = int(os.getenv("ADMIN_ID"))
+CHAT_ID = int(os.getenv("CHAT_ID"))
 DATABASE_NAME = os.getenv("DATABASE_NAME")
+INITIAL_ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 
 telegram_client = TelegramClient('aaa', '23382905', 'e461dd337c4a9c41578cd48c0e9ab3de', proxy=(
@@ -17,8 +18,8 @@ telegram_client = TelegramClient('aaa', '23382905', 'e461dd337c4a9c41578cd48c0e9
 
 token = 'bot56443:49f2b2c3-047a-4897-a5c8-0236f2622680'
 # token = 'bot83779:55b9fbae-1a76-4731-9add-173dbc02b839'
-chat_id = 'testtest11'
-replacement_text = '@test'
+# chat_id = 'testtest11'
+# replacement_text = '@testtest11'
 
 
 async def create_database():
@@ -27,15 +28,80 @@ async def create_database():
         cursor = await conn.cursor()
         await cursor.execute("""
             CREATE TABLE IF NOT EXISTS active_chats (
-                chat_id INTEGER PRIMARY KEY
+                telegram_chat_id INTEGER,
+                chat_id INTEGER,
+                replacement_text TEXT,
+                PRIMARY KEY (telegram_chat_id, chat_id)
             )
-        """)        
+        """)
+        await cursor.execute("""
+            CREATE TABLE IF NOT EXISTS admins (
+                user_id INTEGER PRIMARY KEY
+            )
+        """)
         await conn.commit()
         await conn.close()
     except Exception as e:
-            print("Error in create_database: " + e)
+        print("Error in create_database: " + str(e))
+
 
 asyncio.run(create_database())
+
+
+async def add_admin(user_id):
+    try:
+        conn = await aiosqlite.connect(DATABASE_NAME)
+        cursor = await conn.cursor()
+        await cursor.execute("INSERT OR IGNORE INTO admins (user_id) VALUES (?)", (user_id,))
+        await conn.commit()
+        await conn.close()
+    except Exception as e:
+        print("Error in add_admin: " + str(e))
+
+
+async def remove_admin(user_id):
+    try:
+        conn = await aiosqlite.connect(DATABASE_NAME)
+        cursor = await conn.cursor()
+        await cursor.execute("DELETE FROM admins WHERE user_id = ?", (user_id,))
+        await conn.commit()
+        await conn.close()
+    except Exception as e:
+        print("Error in remove_admin: " + str(e))
+
+
+@telegram_client.on(events.NewMessage(pattern='/addadmin'))
+async def add_admin_command(event):
+    if event.message.sender.id == INITIAL_ADMIN_ID:
+        try:
+            user_id_to_add = int(event.message.text.split()[1])
+            await add_admin(user_id_to_add)
+            await event.respond(f'User {user_id_to_add} has been added as an admin.')
+        except Exception as e:
+            print("Error in addadmin command: " + str(e))
+
+
+@telegram_client.on(events.NewMessage(pattern='/removeadmin'))
+async def remove_admin_command(event):
+    if event.message.sender.id == INITIAL_ADMIN_ID:
+        try:
+            user_id_to_remove = int(event.message.text.split()[1])
+            await remove_admin(user_id_to_remove)
+            await event.respond(f'User {user_id_to_remove} has been removed from admins.')
+        except Exception as e:
+            print("Error in removeadmin command: " + str(e))
+
+
+async def is_admin(user_id):
+    try:
+        conn = await aiosqlite.connect(DATABASE_NAME)
+        cursor = await conn.cursor()
+        await cursor.execute("SELECT 1 FROM admins WHERE user_id = ?", (user_id,))
+        result = await cursor.fetchone()
+        await conn.close()
+        return result is not None
+    except Exception as e:
+        print("Error in is_admin: " + str(e))
 
 
 async def contains_link(message):
@@ -47,116 +113,146 @@ async def get_active_chats():
     try:
         conn = await aiosqlite.connect(DATABASE_NAME)
         cursor = await conn.cursor()
-        await cursor.execute("SELECT chat_id FROM active_chats")
-        active_chats = [row[0] for row in await cursor.fetchall()]
+        await cursor.execute("SELECT telegram_chat_id, chat_id, replacement_text FROM active_chats")
+        active_chats = [(row[0], row[1], row[2]) for row in await cursor.fetchall()]
         await conn.close()
         return active_chats
     except Exception as e:
-            print("Error in get_active_chat: " + e)
+        print("Error in get_active_chat: " + str(e))
+
 
 active_chats = asyncio.run(get_active_chats())
 
 
 @telegram_client.on(events.NewMessage(pattern='/addchat'))
 async def add_chat(event):
+    if not await is_admin(event.message.chat_id) and event.message.sender_id != INITIAL_ADMIN_ID:
+        return
     if event.message.chat_id == CHAT_ID:
-        try :
-            chat_to_add = event.message.text.split()[1]
-            chat_entity = await telegram_client.get_entity(chat_to_add)
-            chat_id_to_add = chat_entity.id
-            chat_id_to_add = int('-100' + str(chat_id_to_add))
+        try:
+            chat_to_add, chat_id, * \
+                replacement_text = event.message.text.split()[1:]
+            replacement_text = ' '.join(
+                replacement_text) if replacement_text else ''
+            try:
+                chat_entity = await telegram_client.get_entity(chat_to_add)
+            except Exception as e:
+                await event.respond(f"There is no Chat as **{chat_to_add}**")
+                return
+            telegram_chat_id_to_add = chat_entity.id
+            telegram_chat_id_to_add = int(
+                '-100' + str(telegram_chat_id_to_add))
             conn = await aiosqlite.connect(DATABASE_NAME)
             cursor = await conn.cursor()
-            await cursor.execute("INSERT OR IGNORE INTO active_chats (chat_id) VALUES (?)", (chat_id_to_add,))
+            await cursor.execute("INSERT OR IGNORE INTO active_chats (telegram_chat_id, chat_id, replacement_text) VALUES (?, ?, ?)", (telegram_chat_id_to_add, chat_id, replacement_text))
             await conn.commit()
             await conn.close()
-            active_chats.append(chat_id_to_add)
-            await event.respond(f'Chat {chat_to_add} has been added.')
+            active_chats.append(
+                (telegram_chat_id_to_add, chat_id, replacement_text))
+            await event.respond(f'Chat **{chat_to_add}** has been added to **{chat_id}** with replacement text "{replacement_text}".')
         except Exception as e:
-            print("Error in addchat: " + e)
+            print("Error in addchat: " + str(e))
 
 
 @telegram_client.on(events.NewMessage(pattern='/deletechat'))
 async def delete_chat(event):
+    if not await is_admin(event.message.chat_id) and event.message.sender_id != INITIAL_ADMIN_ID:
+        return
     if event.message.chat_id == CHAT_ID:
-        try :
-            chat_to_delete = event.message.text.split()[1]
+        try:
+            chat_to_delete, chat_id = event.message.text.split()[1:3]
             chat_entity = await telegram_client.get_entity(chat_to_delete)
-            chat_id_to_delete = chat_entity.id
-            chat_id_to_delete = int('-100' + str(chat_id_to_delete))
+            telegram_chat_id_to_delete = chat_entity.id
+            telegram_chat_id_to_delete = int(
+                '-100' + str(telegram_chat_id_to_delete))
             conn = await aiosqlite.connect(DATABASE_NAME)
             cursor = await conn.cursor()
-            await cursor.execute("DELETE FROM active_chats WHERE chat_id = ?", (chat_id_to_delete,))
+            await cursor.execute("DELETE FROM active_chats WHERE telegram_chat_id = ? AND chat_id = ?", (telegram_chat_id_to_delete, chat_id))
             await conn.commit()
             await conn.close()
-            active_chats.remove(chat_id_to_delete)
-            await event.respond(f'Chat {chat_to_delete} has been removed.')
+            active_chats.remove((telegram_chat_id_to_delete, chat_id))
+            await event.respond(f'Chat {chat_to_delete} has been removed from {chat_id}.')
         except Exception as e:
-            print("Error in deletechat: " + e)
+            print("Error in deletechat: " + str(e))
+
+
+@telegram_client.on(events.NewMessage(pattern='/checkchats'))
+async def check_chats(event):
+    if not await is_admin(event.message.chat_id) and event.message.sender_id != INITIAL_ADMIN_ID:
+        return
+    if event.message.chat_id == CHAT_ID:
+        try:
+            conn = await aiosqlite.connect(DATABASE_NAME)
+            cursor = await conn.cursor()
+            await cursor.execute("SELECT telegram_chat_id, chat_id FROM active_chats")
+            active_chats = [(row[0], row[1]) for row in await cursor.fetchall()]
+            await conn.close()
+
+            response_message = 'Active chats:\n'
+
+            for telegram_chat_id, chat_id in active_chats:
+                response_message += f'Telegram Chat ID: {telegram_chat_id}, Chat ID: {chat_id}\n'
+
+            await event.respond(response_message)
+
+        except Exception as e:
+            print("Error in checkchats: " + str(e))
 
 
 @telegram_client.on(events.NewMessage())
 async def telegram_event_handler(event):
-    if event.message.chat_id not in active_chats:
+    if event.message.chat_id not in [row[0] for row in active_chats]:
         return
 
-    message = event.message.text
+    for chat in active_chats:
+        if chat[0] == event.message.chat_id:
+            chat_id = chat[1]
+            replacement_text = chat[2]
+            message = event.message.text
 
-    if await contains_link(message):
-        return
+            if '@' in message and replacement_text:
+                message = re.sub(r'@\w+', replacement_text, message)
 
-    if event.message.grouped_id:
-        return
+            if event.message.media:
+                file_path = await telegram_client.download_media(event.message)
+                url = f'https://eitaayar.ir/api/{token}/sendFile'
+                headers = {
+                    'Content-Type': 'multipart/form-data'
+                }
+                data = {
+                    'chat_id': chat_id,
+                    'caption': message,
+                    'date': time.time()
+                }
+                try:
+                    with open(file_path, 'rb') as f:
+                        files = {'file': f}
+                        response = requests.post(url, data=data, files=files)
+                except Exception as e:
+                    print("Error in request for send text message with file: " + str(e))
+                finally:
+                    os.remove(file_path)
+                print(response.json())
 
-    if re.search(r'http\S+|www.\S+', message):
-        return
+            else:
 
-    if '@' in message:
-        message = re.sub(r'@\w+', replacement_text, message)
+                url = f'https://eitaayar.ir/api/{token}/sendMessage'
 
-    if event.message.media:
-        file_path = await telegram_client.download_media(event.message)
-        files = {'file': open(file_path, 'rb')}
+                headers = {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
 
-        url = f'https://eitaayar.ir/api/{token}/sendFile'
+                data = {
+                    'chat_id': chat_id,
+                    'text': message,
+                    'date': time.time()
+                }
+                try:
+                    response = requests.post(url, headers=headers, data=data)
+                except Exception as e:
+                    print("Error in request for send text message without file: " + e)
 
-        headers = {
-            'Content-Type': 'multipart/form-data'
-        }
-
-        data = {
-            'chat_id': chat_id,
-            'caption': message,
-            'date': time.time()
-        }
-        try:
-            response = requests.post(url, data=data, files=files)
-        except Exception as e:
-            print("Error in request for send text message with file: " + e)
-
-        files['file'].close()
-        os.remove(file_path)
-        print(response.json())
-
-    else:
-
-        url = f'https://eitaayar.ir/api/{token}/sendMessage'
-
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-
-        data = {
-            'chat_id': chat_id,
-            'text': message,
-            'date': time.time()
-        }
-        try:
-            response = requests.post(url, headers=headers, data=data)
-        except Exception as e:
-            print("Error in request for send text message without file: " + e)
-
-        print(response.json())
+                print(response.json())
 
 
 async def main():
